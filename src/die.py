2d357e2ff90f086e0e32e9cbe21324b37e4a41ef
Dylan Fritz <dfritz1211@gmail.com>
2025-04-18 21:41:20 -0500

    Finish die class

--- src/die.py
+++ src/die.py
@@ -1,15 +1,25 @@
+import random
+
 class Die:
     id = 0
     __init__(self, sides: int):
-    self.sides = sides
-    self.id = Die.id
-    Die.id += 1
-    currentSide = 1
+        self.sides = sides
+        self.id = Die.id
+        Die.id += 1
+        self.currentSide = 1
 
     def printStatus(self):
         print(f"Die id #{self.id}")
         print(f"Number of sides: {self.sides}")
         print(f"Side up: {self.currentSide}")
+
+    def roll(self):
+        self.currentSide = random.randint(1,self.sides)
+        return self.currentSide
+
+    def setSide(self, num):
+        assert num <= self.sides, "Cannot set die to side it does not have"
+        self.currentSide = num
 
 
 
